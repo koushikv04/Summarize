c5a82e52a013f454f315cd77bc0b9db8f359390b
kaushik venkataraman <koushikv@outlook.com>
2025-02-23 19:17:32 +0000

    Initial Commit

---
+++ Summary/SummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryApp.swift
+//  Summary
+//
+//  Created by Kouv on 23/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
